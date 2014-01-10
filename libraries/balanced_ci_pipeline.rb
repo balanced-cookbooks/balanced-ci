4fabf0d0c5b703acea18c5d3ef0bebe28db6652e
Noah Kantrowitz <noah@coderanger.net>
2014-01-09 16:59:20 -0800

    Update header.

--- libraries/balanced_ci_pipeline.rb
+++ libraries/balanced_ci_pipeline.rb
@@ -1,7 +1,8 @@
 #
 # Author:: Noah Kantrowitz <noah@coderanger.net>
+# Author:: Marshall Jones <marshall@balancedpayments.com>
 #
-# Copyright 2013, Balanced, Inc.
+# Copyright 2013-2014, Balanced, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
