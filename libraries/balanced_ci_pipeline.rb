79ff35fefb202e517d76082c8cdb33db42b736ef
Noah Kantrowitz <noah@coderanger.net>
2014-01-24 19:09:18 -0800

    Fix the not_if for the key import.

--- libraries/balanced_ci_pipeline.rb
+++ libraries/balanced_ci_pipeline.rb
@@ -217,7 +217,7 @@ class Chef
         end
         execute 'gpg --import /root/packages@vandelay.io.pem' do
           user 'root'
-          not_if 'gpg --list-secret-keys 277E7787'
+          not_if 'env HOME=/root gpg --list-secret-keys 277E7787'
           environment 'HOME' => Dir.home('root') # Because GPG uses $HOME instead of real home
         end
       end
