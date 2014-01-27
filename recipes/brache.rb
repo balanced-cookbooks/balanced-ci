ca4ef95e45130cc6dae8a153a4580fdbab9059cf
bninja <andrew@poundpay.com>
2014-01-27 15:36:09 -0800

    seems to work

--- recipes/brache.rb
+++ recipes/brache.rb
@@ -2,9 +2,10 @@ balanced_ci_pipeline 'brache' do
 
   repository 'git@github.com:balanced/brache.git'
   cookbook_repository 'git@github.com:balanced-cookbooks/role-balanced-auth.git'
-  pipeline %w{test quality build acceptance}
   project_url 'https://github.com/balanced/brache'
   branch 'master'
+
+  pipeline %w{test quality build acceptance}
 
   job 'test' do |new_resource|
     builder_recipe do
@@ -12,30 +13,37 @@ balanced_ci_pipeline 'brache' do
       include_recipe 'python'
       include_recipe 'balanced-python'
       include_recipe 'postgresql::client'
-      include_recipe 'rabbitmq::client'
-      
-      template '/srv/ci/.pydistutils.cfg' do
-        cookbook 'balanced-devpi'
-        owner 'jenkins'
-        group 'jenkins'
-        mode '600'
-        source 'pydistutils.cfg.erb'
-        variables password: 'EnkgU5nr4sdR5zwz'
+
+      group node['jenkins']['node']['group']
+
+      user node['jenkins']['node']['user'] do
+        comment 'Jenkins CI node'
+        gid node['jenkins']['node']['group']
+        home node['ci']['path']
       end
 
+      directory node['ci']['path'] do
+        owner node['jenkins']['node']['user']
+        group node['jenkins']['node']['group']
+      end
+
+      template "#{node['ci']['path']}/.pydistutils.cfg" do
+        cookbook 'balanced-devpi'
+        owner node['jenkins']['node']['user']
+        group node['jenkins']['node']['group']
+        mode '600'
+        source 'pydistutils.cfg.erb'
+        variables password: citadel['omnibus/devpi_password'].strip
+      end
     end
   end
 
-  test_command <<-COMMAND.gsub(/^ {4}/, '')  
+  test_command <<-COMMAND.gsub(/^ {4}/, '')
     python setup.py develop easy_install brache[user,test,router]
     nosetests -v -s --with-id --with-xunit --with-xcoverage --cover-package=brache--cover-erase
   COMMAND
-  
+
   quality_command 'coverage.py coverage.xml brache:50'
-  
-  job 'acceptance' do
-    branch 'berks3'
-  end
 
 end
 
