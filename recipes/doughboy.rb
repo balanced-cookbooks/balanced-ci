0d27a1732fde989f7a1550509db46f6d2aa73285
Victor Lin <bornstub@gmail.com>
2014-02-22 10:49:37 +0800

    Update doghboy project url

--- recipes/doughboy.rb
+++ recipes/doughboy.rb
@@ -21,6 +21,6 @@ balanced_ci_pipeline 'doughboy' do
   repository 'git@github.com:balanced/doughboy.git'
   cookbook_repository 'git@github.com:balanced-cookbooks/role-doughboy.git'
   pipeline %w{build acceptance}
-  project_url 'https://github.com/balanced/invoice_feeder'
+  project_url 'https://github.com/balanced/doughboy'
   branch 'master'
 end
