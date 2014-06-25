006ffac6f1d15f24b0abfe3a945a876beaa079ad
Victor Lin <bornstub@gmail.com>
2014-06-25 15:13:16 -0700

    add missing docker repo url

--- attributes/default.rb
+++ attributes/default.rb
@@ -39,6 +39,7 @@ override['jenkins']['node']['home'] = '/var/lib/jenkins'
 
 # FIXME: just for temporary development only, we may should register a proper
 # account later
+default['balanced-docker']['repo_url'] = 'https://index.docker.io/v1/'
 default['balanced-docker']['email'] = 'victorlin+docker.balanceddeploy@balancedpayments.com'
 default['balanced-docker']['username'] = 'balanceddeploy'
 default['balanced-docker']['password_file'] = 'docker/balanceddeploy_password'
